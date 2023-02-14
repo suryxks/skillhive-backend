c5b1a325a4d21dbb4a248c1a88747fcf4fe36579
Surya <123004250@sastra.ac.in>
2023-02-14 17:00:36 +0530

    update: fixed handleinputerror

--- src/middlewares/handleInputErrors.ts
+++ src/middlewares/handleInputErrors.ts
@@ -4,5 +4,7 @@ export const handleInputErrors = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).json({errors:errors.array()})
+    } else {
+        next()
     }
 }
