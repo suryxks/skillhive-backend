0748979de2fb15151162e9be07713a76139f3599
Surya <123004250@sastra.ac.in>
2023-02-17 16:34:25 +0530

    fix: fixed auth middleware and changes in db schema

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,7 @@
 import { verifyToken } from '../utils';
 export const auth = (req,res,next) => {
-    const { user,token } = req.body;
+    const { user } = req.body;
+    const token = req.headers.authorization;
     if (token && verifyToken(token)) {
         req.user = user;
         next();
