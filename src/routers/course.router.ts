d5dc8467cb860d18010e8d3cf42fda9957f325d9
Surya <123004250@sastra.ac.in>
2023-02-20 09:12:10 +0530

    feat: added get and edit course

--- src/routers/course.router.ts
+++ src/routers/course.router.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import prisma from "../db";
-import { body } from "express-validator";
+import { body ,param} from "express-validator";
 import { handleInputErrors } from "../middlewares";
 
 const router = express.Router();
@@ -39,5 +39,46 @@ router.post(
       
   }
 );
+router.get('/:courseId',
+    param('courseId'),
+    handleInputErrors,
+    async (req, res) => {
+        const { courseId } = req.params;
+        try {
+            const course = await prisma.course.findUnique({
+                where: {
+                    id: courseId,
+                }
+            })
+            res.status(200).json({data:{course}})
+        } catch (error) {
+            console.error(error)
+        }
+    }
+)
+router.post('/:courseId',
+param('courseId'),
+    body('name').isString(),
+body('description').isString(),
+handleInputErrors,
+async (req, res) => {
+    const { courseId } = req.params;
+    const { name,description } = req.body;
+    try {
+        const course = await prisma.course.update({
+            where: {
+                id: courseId,
+            },
+            data: {
+                name: name,
+                description:description
+            }
+        })
+          res.status(200).json({data:course})
+        
+    } catch (error) {
+        console.error(error)
+    }
+})
 
 export { router as courseRouter };
